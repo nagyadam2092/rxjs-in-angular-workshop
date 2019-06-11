65295508e28725c0e39582c7eea6875573653b63
nagyadam2092 <nagyadam2092@gmail.com>
2019-06-11 19:13:51 +0200

    exercise 3 solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { interval } from 'rxjs';
+import { filter } from 'rxjs/internal/operators/filter';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -9,5 +11,9 @@ import { interval } from 'rxjs';
 export class AppComponent {
     title = 'angular-rxjs-workshop';
 
-    source$ = interval(1000);
+    source$ = interval(1000)
+        .pipe(
+            filter(elem => elem % 2 === 0),
+            map(elem => elem * 10)
+        );
 }
